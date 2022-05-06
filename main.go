f29997930b91393f9aafa78371f6a6923c031e9e
MNoun <mnoun@student.bridgew.edu>
2022-05-06 13:49:53 -0400

    Added functions outlining requirements

--- main.go
+++ main.go
@@ -2,6 +2,64 @@
 Created by: Mitchell Noun
 Date created: 4/29/22
 Class: COMP415 Emerging Languages
-Assignment: Final Project: Project 3
+Assignment: Final Project: Project 4
 *************************/
 package main
+
+func main() {
+
+}
+
+//creates database table
+func databaseSetup() {
+
+}
+
+//run once, inserts excel data into database
+func populateDatabase() {
+
+}
+
+/*
+Below are separate functions to get required excel data
+*/
+
+func getExcelName() {
+
+}
+
+func getExcelAge() {
+
+}
+
+func getExcelDLC() {
+
+}
+
+func getExcelMetacritic() {
+
+}
+
+func getExcelRecCount() {
+
+}
+
+func getExcelSteamOwners() {
+
+}
+
+func getExcelSteamPlayers() {
+
+}
+
+func getExcelPLinux() {
+
+}
+
+func getExcelPMac() {
+
+}
+
+func getExcelPWindows() {
+
+}
